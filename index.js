c5dd03d0164f5c73aaf7b1cdd8e4796a3bd72b41
Rajvir Singh <57231050+rajvirsiingh@users.noreply.github.com>
2022-07-08 23:07:22 +0530

    Files Upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".share-icon").addEventListener("click", function () {
+  document.querySelector(".links").classList.toggle("show");
+  document.querySelector(".links").classList.toggle("hide");
+});
